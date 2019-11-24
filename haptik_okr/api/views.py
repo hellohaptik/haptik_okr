8c32f9af777c3e2ad611cce6812e67078d93275c
rajas.ashtikar <rajas.ashtikar@haptik.co>
2019-11-24 17:05:14 +0530

    Add basic flow for login and signup
    - Add urls for login and signup
    - Signup user as default django user

--- haptik_okr/api/views.py
+++ haptik_okr/api/views.py
@@ -1,3 +1,50 @@
+import json
+
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from rest_framework import generics
+
+
+class LoginView(generics.CreateAPIView):
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get("username", "")
+        password = request.data.get("password", "")
+        user_obj = authenticate(request, username=username, password=password)
+
+        # if username or password is empty throw error
+        if not username or not password:
+            return HttpResponse(json.dumps({'status': False, 'error_code': 400, 'error_message': 'Username or password '
+                                                                                                 'cannot be blank'}),
+                                content_type='json')
+
+        if user_obj is not None:
+            return HttpResponse("User logged in")
+        else:
+            return HttpResponse("No such user exists")
+
+
+class SignupView(generics.CreateAPIView):
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get("username", "")
+        password = request.data.get("password", "")
+        email = request.data.get("email", "")
+
+        if not username or not password or not email:
+            return HttpResponse(json.dumps({'status': False, 'error_code': 400, 'error_message': 'Username or '
+                                                                                                 'password or email '
+                                                                                                 'cannot be blank'}),
+                                content_type='json')
+
+        user_obj = User.objects.create_user(username=username.lower(), email=email.lower(), password=password)
+        if user_obj is not None:
+            user_obj.save()
+            return HttpResponse("User created successfully")
+        else:
+            return HttpResponse("Failed to create user")

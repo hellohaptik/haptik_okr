d358ae61404aae12e30e91c1f00ac1127a3d2576
tejas-haptik <tejas.sakpal@haptik.co>
2019-11-26 15:32:27 +0530

    Add model files for OKR's (#5)
    Added the following model files:
    
    Objective
    KeyResults
    Sheet
    Teams

---
+++ haptik_okr/api/models/user_related.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=250, null=False)
+    head = models.ForeignKey(User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)

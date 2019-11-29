6ae63e8e3777e3b871e1c7d90e51f90360389842
Rajas Ashtikar <39483191+haptikrajasashtikar@users.noreply.github.com>
2019-11-29 16:08:59 +0530

    Add a Model `Quarter` (#9)
    - Add a new model `Quarter` to the DB

--- haptik_okr/api/models/okr_related.py
+++ haptik_okr/api/models/okr_related.py
@@ -4,11 +4,18 @@ from django.db import models
 from api.models import Team
 
 
-class Sheet(models.Model):
-    team_id = models.ForeignKey(Team, on_delete=models.PROTECT)
+class Quarter(models.Model):
     quarter_name = models.CharField(max_length=100, null=False)
     quarter_start_date = models.DateField()
     quarter_end_date = models.DateField()
+    is_current = models.BooleanField
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class Sheet(models.Model):
+    team_id = models.ForeignKey(Team, on_delete=models.PROTECT)
+    quarter_id = models.ForeignKey(Quarter, on_delete=models.PROTECT)
     progress = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)

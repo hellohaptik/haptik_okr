25d4acfb2d82009107a5a257e608be65452d712b
ankur.jain <ankur.jain@haptik.co>
2019-12-04 19:06:15 +0530

    Added Login UI for OKR tool
    Added Login UI for OKR tool

---
+++ frontend/haptik_okr/src/loginview.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(16),
+    display: 'flex',
+    padding:'10px',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 3),
+  },
+  parent: {
+    backgroundColor: 'white',
+  }
+}));
+
+export default function SignIn() {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs" borderRadius={16} className={classes.parent}>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <form className={classes.form}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Grid
+            container
+            justify="center"
+            direction="column"
+            alignItems="center">
+            <Button
+              type="submit"
+              variant="contained"
+              size="large"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+
+            <Link href="#"
+              underline="always"
+            >
+              {"Forgot password?"}
+            </Link>
+
+            <Link href="#"
+              underline="always"
+              className={classes.submit}
+            >
+              {"Don't have an account? Sign Up"}
+            </Link>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+}

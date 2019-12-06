994ff93f223a065e37d8271393e38b603ccdc9a0
Rajas Ashtikar <39483191+haptikrajasashtikar@users.noreply.github.com>
2019-12-06 16:51:49 +0530

    Add keys for api response (#15)

--- haptik_okr/api/views_homescreen.py
+++ haptik_okr/api/views_homescreen.py
@@ -1,50 +1,58 @@
-import json
-from django.http import HttpResponse
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from api.models.okr_related import Quarter
+from api.okr_decorators import send_api_response
+from api.exceptions import APIError
+import datetime
 
 
 def populate_quarter_data(quarter_list):
     all_quarter_data = []
-    if len(quarter_list) == 1:
-        data = {
-            'id': quarter_list[0].id,
-            'name': quarter_list[0].quarter_name,
-            'quarter_start_date': quarter_list[0].quarter_start_date.strftime('%m/%d/%Y'),
-            'quarter_end_date': quarter_list[0].quarter_end_date.strftime('%m/%d/%Y'),
-            'is_current': quarter_list[0].is_current
-        }
-
-        return data
-
+    response = {'quarters': None}
     for quarter in quarter_list:
-        data = {
-            'id': quarter.id,
-            'name': quarter.quarter_name,
-            'quarter_start_date': quarter.quarter_start_date.strftime('%d/%m/%Y'),
-            'quarter_end_date': quarter.quarter_end_date.strftime('%d/%m/%Y'),
-            'is_current': quarter.is_current
-        }
-        all_quarter_data.append(data)
-    return all_quarter_data
+        if quarter.quarter_start_date < datetime.date.today():
+            data = {
+                'id': quarter.id,
+                'name': quarter.quarter_name,
+                'quarter_start_date': quarter.quarter_start_date.strftime('%d/%m/%Y'),
+                'quarter_end_date': quarter.quarter_end_date.strftime('%d/%m/%Y'),
+                'is_current': quarter.is_current
+            }
+            all_quarter_data.append(data)
+    response['quarters'] = all_quarter_data
+    return response
 
 
-class QuarterView(APIView):
-
-    def get(self, request):
-        quarter_list = []
-        request_params = request.query_params.dict()
-        if len(request_params) > 0:
+@api_view()
+@send_api_response
+def get_all_or_current_quarter(request):
+    quarter_list = []
+    request_params = request.query_params.dict()
+    if len(request_params) > 0:
+        try:
             if request_params.get('is_current') is not None:
-                is_current = request_params.get('is_current')
-                if str.lower(is_current) == 'true':
+                is_current = int(request_params.get('is_current'))
+                if is_current == 1:
                     quarter_list = Quarter.objects.filter(is_current=True).order_by('quarter_start_date')
-                elif str.lower(is_current) == 'false':
+                elif is_current == 0:
                     quarter_list = Quarter.objects.filter(is_current=False)
-            else:
-                # TODO: properly formatted error will be sent in subsequent PRs
-                return HttpResponse("Invalid request")
-        else:
-            quarter_list = Quarter.objects.all()
+        except ValueError as e:
+            raise APIError(message='Invalid Request', status=400)
+    else:
+        quarter_list = Quarter.objects.all()
 
-        return HttpResponse(json.dumps(populate_quarter_data(quarter_list)))
+    return populate_quarter_data(quarter_list)
+
+
+@api_view()
+@send_api_response
+def get_quarter_by_id(request, quarter_id):
+    try:
+        q_id = int(quarter_id)
+        if q_id < 0:
+            raise APIError(message='Invalid Id', status=400)
+        else:
+            quarter_list = Quarter.objects.filter(id=q_id)
+            return populate_quarter_data(quarter_list)
+
+    except ValueError as e:
+        raise APIError(message='Quarter id should be an integer', status=400)

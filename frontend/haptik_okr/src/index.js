0a33552f5ebdfa7dbc691e72946295894267e851
Simranjot <iamsimranjot@gmail.com>
2019-12-11 14:07:23 +0530

    Project structure updated (#19)
    * Project structure updated
    
    * Removed unused css

--- frontend/haptik_okr/src/index.js
+++ frontend/haptik_okr/src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import UserRegistrationView from "./signup/userRegistrationView";
-import UserLoginView from "./signup/userLoginView";
+import LoginView from "./components/LoginView";
+import SignupView from "./components/SignupView";
 import * as serviceWorker from "./serviceWorker";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 
 const routing = (
   <Router>
     <div>
-      <Route exact path="/" component={UserLoginView} />
-      <Route exact path="/registration" component={UserRegistrationView} />
+      <Route exact path="/" component={LoginView} />
+      <Route exact path="/registration" component={SignupView} />
     </div>
   </Router>
 );

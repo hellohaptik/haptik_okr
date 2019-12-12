97fa1f95d1c32d9b6b64454edc57837a5a9b9855
iamsimranjot <iamsimranjot@gmail.com>
2019-12-12 21:24:54 +0530

    Added Header view

--- frontend/haptik_okr/src/index.js
+++ frontend/haptik_okr/src/index.js
@@ -1,17 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+
 import LoginView from "./containers/LoginView";
 import SignupView from "./containers/SignupView";
-import * as serviceWorker from "./serviceWorker";
+import Header from "./components/Header";
+
 import { Route, BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+import blueGrey from "@material-ui/core/colors/blueGrey";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blueGrey
+  },
+  typography: {}
+});
 
 const routing = (
-  <Router>
-    <div>
-      <Route exact path="/" component={LoginView} />
-      <Route exact path="/registration" component={SignupView} />
-    </div>
-  </Router>
+  <ThemeProvider theme={theme}>
+    <Router>
+      <div>
+        <Route exact path="/" component={Header} />
+        <Route exact path="/registration" component={SignupView} />
+      </div>
+    </Router>
+  </ThemeProvider>
 );
 
 ReactDOM.render(routing, document.getElementById("root"));

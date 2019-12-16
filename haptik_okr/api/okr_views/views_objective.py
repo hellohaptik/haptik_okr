e43069480632c240df82e8f283e75131c396927c
Rajas Ashtikar <39483191+haptikrajasashtikar@users.noreply.github.com>
2019-12-16 15:59:43 +0530

    Add endpoint for updating Objective (#29)
    * Add endpoint for updating Objective
    
    - Add endpoint to update the objective
    
    * Make changes requested in PR

--- haptik_okr/api/okr_views/views_objective.py
+++ haptik_okr/api/okr_views/views_objective.py
@@ -7,8 +7,20 @@ from api.models.okr_related import Objective, Sheet
 from api.utils import validate_request_parameters
 from api.exceptions import APIError
 from api.okr_decorators import send_api_response
-import api.constants
+from api.constants import INVALID_REQUEST
 from django.core.exceptions import ObjectDoesNotExist
+
+
+def populate_response_data(objective):
+    api_response = {}
+    data = {
+        'id': objective.id,
+        'sheet_id': objective.quarter_sheet_id,
+        'title': objective.title,
+        'progress': objective.progress
+    }
+    api_response['objective'] = data
+    return api_response
 
 
 class ObjectiveView(APIView):
@@ -16,9 +28,7 @@ class ObjectiveView(APIView):
     @method_decorator(send_api_response)
     def post(self, request):
         # TODO: check if user can create the Objective based on the information in header
-        api_response = {'objective': None}
-
-        request_body = json.loads(request.body.decode(encoding='UTF-8'))
+        request_body = json.loads(request.body)
         valid, response = validate_request_parameters(request, ['sheet_id', 'title'])
         if valid:
             try:
@@ -26,15 +36,31 @@ class ObjectiveView(APIView):
                 title = str(request_body.get('title'))
                 sheet = Sheet.objects.get(pk=sheet_id)
                 objective = Objective.objects.create(title=title, quarter_sheet=sheet, progress=0)
-                data = {
-                    'id': objective.id,
-                    'sheet_id': sheet_id,
-                    'title': objective.title,
-                    'progress': objective.progress
-                }
-                api_response['objective'] = data
-                return api_response
+                return populate_response_data(objective)
             except (ValueError, ObjectDoesNotExist) as e:
-                raise APIError(message=api.constants.INVALID_REQUEST, status=400)
+                raise APIError(message=INVALID_REQUEST, status=400)
+        else:
+            raise APIError(message=response, status=400)
+
+
+class ObjectiveDetailView(APIView):
+
+    @method_decorator(send_api_response)
+    def put(self, request, objective_id):
+        # TODO: check if user can update the Objective based on the information in header
+        request_body = json.loads(request.body)
+        valid, response = validate_request_parameters(request, ['title'])
+        if valid:
+            try:
+                # TODO explore serializers and try to optimize this code, create a object for every post, put request
+                #  and try and serialize requests into that also for the responses
+                objective_id = int(objective_id)
+                objective_title = str(request_body.get('title'))
+                objective = Objective.objects.get(pk=objective_id)
+                objective.title = objective_title
+                objective.save()
+                return populate_response_data(objective)
+            except (ValueError, Objective.DoesNotExist) as e:
+                raise APIError(message=INVALID_REQUEST, status=400)
         else:
             raise APIError(message=response, status=400)

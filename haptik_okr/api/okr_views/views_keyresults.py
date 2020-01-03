b2f998d3d78c385b7503f80925a98ac7b86cfc34
Rajas Ashtikar <39483191+haptikrajasashtikar@users.noreply.github.com>
2020-01-03 15:52:58 +0530

    Add put call for sheet (#33)

--- haptik_okr/api/okr_views/views_keyresults.py
+++ haptik_okr/api/okr_views/views_keyresults.py
@@ -2,17 +2,20 @@ from django.utils.decorators import method_decorator
 from rest_framework import generics
 from rest_framework.views import APIView
 
-from api.okr_decorators import send_api_response
+from api.okr_decorators import send_api_response, authenticate_user
 import json
 
-from api.utils import validate_request_parameters
-from api.models.okr_related import Objective, KeyResults
+from api.utils import validate_request_parameters, check_user_permission
+from api.models.okr_related import Objective, KeyResults, Sheet
 from api.exceptions import APIError
-from api.constants import INVALID_REQUEST
+from api.constants import INVALID_REQUEST, UNAUTHORIZED_REQUEST
+from django.db.models import Avg
+import math
 
 
 # TODO: check if we can write a util method to populate data given a dict of keys and corresponding objects
 def populate_keyresults_data(keyresult):
+    sheet_progress, objective_progress = get_sheet_and_objective_progress(keyresult)
     api_response = {}
     data = {
         'id': keyresult.id,
@@ -22,11 +25,29 @@ def populate_keyresults_data(keyresult):
         'objective_id': keyresult.objective_id
     }
     api_response['keyresult'] = data
+    api_response['objective_progress'] = objective_progress
+    api_response['sheet_progress'] = sheet_progress
     return api_response
 
 
-class KeyResultsView(generics.CreateAPIView):
+def check_permissions(request, objective_id):
+    objective = Objective.objects.get(pk=objective_id)
+    valid = check_user_permission(request, objective.quarter_sheet_id)
+    if not valid:
+        raise APIError(message=UNAUTHORIZED_REQUEST, status=401)
 
+
+def get_sheet_and_objective_progress(keyresult):
+    if keyresult is not None:
+        objective_id = keyresult.objective_id
+        objective = Objective.objects.get(pk=objective_id)
+        sheet_id = objective.quarter_sheet_id
+        sheet = Sheet.objects.get(pk=sheet_id)
+        return sheet.progress, objective.progress
+
+
+class KeyResultsView(generics.CreateAPIView):
+    @method_decorator(authenticate_user)
     @method_decorator(send_api_response)
     def post(self, request, *args, **kwargs):
         # TODO: check if user can create the task based on the information in header
@@ -35,6 +56,7 @@ class KeyResultsView(generics.CreateAPIView):
         if valid:
             try:
                 objective_id = int(request_body.get('objective_id'))
+                check_permissions(request, objective_id)
                 title = str(request_body.get('title'))
                 objective = Objective.objects.get(pk=objective_id)
                 keyresult = KeyResults.objects.create(title=title, objective=objective, progress=0, is_discarded=False)
@@ -45,33 +67,56 @@ class KeyResultsView(generics.CreateAPIView):
 
 
 class KeyResultsDetailsView(APIView):
-
-    @send_api_response
+    @method_decorator(authenticate_user)
+    @method_decorator(send_api_response)
     def put(self, request, keyresult_id):
         # TODO: check if user can create the task based on the information in header
         request_body = json.loads(request.body)
+        progress_updated = False
         try:
             keyresult_id = int(keyresult_id)
             title = str(request_body.get('title'))
             progress = int(request_body.get('progress'))
             keyresult = KeyResults.objects.get(pk=keyresult_id)
+            check_permissions(request, keyresult.objective_id)
             if title:
                 keyresult.title = title
             if progress:
-                keyresult.progress = progress
+                if 0 <= progress <= 100:
+                    keyresult.progress = progress
+                    progress_updated = True
+                else:
+                    raise APIError(message='Invalid data', status=400)
             keyresult.save()
-            return populate_keyresults_data(keyresult)
+            if progress_updated:
+                self.update_sheet_progress(keyresult.objective_id)
+            response = populate_keyresults_data(keyresult)
+            return response
         except (ValueError, KeyResults.DoesNotExist, TypeError) as e:
             raise APIError(message=INVALID_REQUEST, status=400)
 
-    @send_api_response
+    @method_decorator(authenticate_user)
+    @method_decorator(send_api_response)
     def delete(self, request, keyresult_id):
-        # TODO: check if user can create the task based on the information in header
         try:
             keyresult_id = int(keyresult_id)
             keyresult = KeyResults.objects.get(pk=keyresult_id)
+            check_permissions(request, keyresult.objective_id)
             keyresult.is_discarded = True
             keyresult.save()
             return 'Keyresult successfully discarded'
         except (ValueError, KeyResults.DoesNotExist) as e:
             raise APIError(message=INVALID_REQUEST, status=400)
+
+    def update_sheet_progress(self, objective_id):
+        objective = Objective.objects.get(pk=objective_id)
+        progress = KeyResults.objects.filter(objective_id=objective_id, is_discarded=False).aggregate(
+            obj_progress=Avg('progress'))
+        objective.progress = math.floor(progress.get('obj_progress'))
+        objective.save()
+
+        sheet_id = objective.quarter_sheet.id
+        sheet = Sheet.objects.get(pk=sheet_id)
+        sheet_progress = Objective.objects.filter(quarter_sheet_id=sheet_id).aggregate(sheet_progress=Avg('progress'))
+        sheet.progress = math.floor(sheet_progress.get('sheet_progress'))
+        sheet.save()
--- haptik_okr/api/okr_views/views_sheet.py
+++ haptik_okr/api/okr_views/views_sheet.py
@@ -2,45 +2,165 @@ from django.utils.decorators import method_decorator
 from rest_framework.views import APIView
 from api.exceptions import APIError
 import api.constants
-from api.models.okr_related import Objective, KeyResults
-from api.okr_decorators import send_api_response
+from api.models.okr_related import Objective, KeyResults, Sheet
+from api.okr_decorators import send_api_response, authenticate_user
+from api.okr_encryption import decrypt_user_id
+from api.utils import get_user_auth_token, check_user_permission
+import json
+from django.db.models import Avg
+import math
+
+PROGRESS = 'progress'
+
+
+def get_sheet_details(sheet_id):
+    response = {}
+    okrs = []
+    sheet = Sheet.objects.get(pk=sheet_id)
+    objective_list = Objective.objects.filter(quarter_sheet_id=sheet_id)
+    objective_id_list = [objective.id for objective in objective_list]
+    keyresults_list = KeyResults.objects.filter(objective__id__in=objective_id_list)
+    for obj in objective_list:
+        krs = []
+        for kr in keyresults_list.filter(objective_id=obj.id):
+            keyresult = {
+                'id': kr.id,
+                'title': kr.title,
+                'progress': kr.progress,
+                'is_discarded': kr.is_discarded
+            }
+            krs.append(keyresult)
+        objective = {
+            'id': obj.id,
+            'title': obj.title,
+            'progress': obj.progress,
+            'keyresults': krs}
+        okrs.append(objective)
+        response['objectives'] = okrs
+        response['sheet_progress'] = sheet.progress
+    return response
 
 
 class SheetView(APIView):
-
+    @method_decorator(authenticate_user)
     @method_decorator(send_api_response)
     def get(self, request):
         request_params = request.query_params.dict()
-        response = {}
-        okrs = []
         if len(request_params) > 0:
             try:
                 if request_params.get('id') is not None:
                     sheet_id = int(request_params.get('id'))
-                    objective_list = Objective.objects.filter(quarter_sheet_id=sheet_id)
-                    objective_id_list = [objective.id for objective in objective_list]
-                    keyresults_list = KeyResults.objects.filter(objective__id__in=objective_id_list)
-                    for obj in objective_list:
-                        krs = []
-                        for kr in keyresults_list.filter(objective_id=obj.id):
-                            keyresult = {
-                                'id': kr.id,
-                                'title': kr.title,
-                                'progress': kr.progress,
-                                'is_discarded': kr.is_discarded
-                            }
-                            krs.append(keyresult)
-                        objective = {
-                            'id': obj.id,
-                            'title': obj.title,
-                            'progress': obj.progress,
-                            'keyresults': krs}
-                        okrs.append(objective)
-                        response['objectives'] = okrs
-                    return response
+                    return get_sheet_details(sheet_id)
                 else:
                     raise APIError(api.constants.INVALID_REQUEST, status=400)
             except ValueError as e:
                 raise APIError(message="Invalid id", status=400)
         else:
             raise APIError(message=api.constants.INVALID_REQUEST, status=400)
+
+
+class SheetDetailView(APIView):
+    @method_decorator(authenticate_user)
+    @method_decorator(send_api_response)
+    def put(self, request, sheet_id):
+        try:
+            # cast the sheet_id to int else raise an exception
+            s_id = int(sheet_id)
+
+            # check if the user allowed to make a put call
+            request_permitted = check_user_permission(request, s_id)
+            if not request_permitted:
+                raise APIError(message="Unauthorized request", status=401)
+
+            # get objectives and keyresults data from db for validations
+            db_objective_list = Objective.objects.filter(quarter_sheet_id=s_id)
+            db_objective_id_list = [objective.id for objective in db_objective_list]
+            db_keyresults_list = KeyResults.objects.filter(objective__id__in=db_objective_id_list)
+
+            # parse the request body into dict
+            request_body = json.loads(request.body)
+
+            # get the list of objectives from the request body
+            req_objective_list = request_body.get('objectives')
+            if req_objective_list:
+                valid_objectives_keyresults = self.validate_objectives_and_keyresults(req_objective_list,
+                                                                                      db_objective_id_list,
+                                                                                      db_keyresults_list)
+                if valid_objectives_keyresults:
+                    self.update_objectives_and_keyresults(req_objective_list, db_objective_list, db_keyresults_list,
+                                                          s_id)
+                    return get_sheet_details(s_id)
+                else:
+                    raise APIError(message="Invalid data", status=400)
+            return "Return a sheet object with updated data of objectives and keyresults"
+        except ValueError as e:
+            raise APIError(message="Invalid sheet id", status=400)
+
+    def validate_objectives_and_keyresults(self, req_objective_list, db_objective_id_list, db_keyresults_list):
+        for req_objective in req_objective_list:
+            # check if the objective in the request belongs to the sheet id sent in the url
+            if req_objective.get('id') not in db_objective_id_list:
+                return False
+            else:
+                objective_id = req_objective.get('id')
+                # get the ids of keyresults for the objective in the request from the db
+                obj_keyresults_ids = db_keyresults_list.filter(objective_id=objective_id).values_list('id', flat=True)
+                # get the keyresults from the request objective object
+                req_obj_keyresults = req_objective.get('keyresults')
+                # check if the keyresults sent as a part of the objective in the request belong to the objective
+                for req_obj_keyresult in req_obj_keyresults:
+                    if req_obj_keyresult.get('id') not in obj_keyresults_ids:
+                        return False
+                    else:
+                        try:
+                            # if progress is not an integer, it will throw a type error, which indicates data is invalid
+                            # if progress key is not present, validations will not run, but data is valid
+                            keyresult_progress = int(req_obj_keyresult.get(PROGRESS))
+                            if not 0 <= keyresult_progress <= 100:
+                                return False
+                        except ValueError as e:
+                            return False
+                        except KeyError as e:
+                            pass
+
+        return True
+
+    def update_objectives_and_keyresults(self, req_objective_list, db_objective_list, db_keyresults_list, sheet_id):
+        for req_objective in req_objective_list:
+
+            # update the objective object
+            db_objective = db_objective_list.get(pk=req_objective.get('id'))
+            db_objective.title = req_objective.get('title')
+            db_objective.save()
+
+            # update the keyresults for the objective
+            req_keyresults_list = req_objective.get('keyresults')
+            for req_keyresult in req_keyresults_list:
+                db_keyresult = db_keyresults_list.get(pk=req_keyresult.get('id'))
+
+                # update the data for db keyresult
+                db_keyresult.title = req_keyresult.get('title')
+                db_keyresult.progress = req_keyresult.get(PROGRESS)
+                db_keyresult.objective = db_objective
+                db_keyresult.save()
+
+            # update the progress for objective
+            # querying keyresults again to ensure updated data is retrieved while saving progress for objective
+            obj_progress = KeyResults.objects.filter(objective_id=db_objective.id, is_discarded=False).aggregate(
+                progress=Avg(PROGRESS))
+            if obj_progress.get(PROGRESS) is not None:
+                db_objective.progress = math.floor(obj_progress.get(PROGRESS))
+            else:
+                db_objective.progress = 0
+            db_objective.save()
+
+        # update progress for sheet
+        sheet = Sheet.objects.get(pk=sheet_id)
+        # querying objectives again to ensure updated data is retrieved while saving progress for sheet
+        sheet_progress = Objective.objects.filter(quarter_sheet_id=sheet_id, is_discarded=False).aggregate(
+            progress=Avg(PROGRESS))
+        if sheet_progress.get(PROGRESS) is not None:
+            sheet.progress = math.floor(sheet_progress.get(PROGRESS))
+        else:
+            sheet.progress = 0
+        sheet.save()
